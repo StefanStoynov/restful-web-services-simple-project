659ddd0ad73dddcd90b749dfc2fa7d46a3b9a404
StefanStoynov <sstoynov87@gmail.com>
2022-02-23 19:14:59 +0200

    Introduce simple rest controller - HelloWorldController

---
+++ src/main/java/com/ss/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.ss.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//tell the Spring that this class is Controller
+@RestController
+public class HelloWorldController {
+    //GET
+    //URI - /hello-world
+    @GetMapping(path = "/hello-world")
+    //method to return hello world
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
