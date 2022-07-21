e06b809c9e1aa82b2d2f8cdb07499d95e09b4915
StefanStoynov <sstoynov87@gmail.com>
2022-07-21 21:39:54 +0300

    Introduce simple JPA classes

---
+++ src/main/java/com/ss/entity/User.java
@@ -0,0 +1,43 @@
+package com.ss.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String role;
+
+    protected User() {
+    }
+
+    public User(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
